8d97328fe40c25a385092b6ba373f617d9d00ff8
Ubuntu <ubuntu@ip-172-31-19-71.eu-north-1.compute.internal>
2023-12-03 15:54:19 +0000

    task_3_rev_01

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python3
+"""
+starting my api
+"""
+
+
 from api.v1.views import app_views
 
 @app_views.route("/status", methods=["GET"])
 def get_status():
     """
-        create a route /status on the object app_views that
-        returns a JSON: "status": "OK"
+    create a route /status on the object app_views that
+    returns a JSON: "status": "OK"
     """
     return {"status": "OK"}
